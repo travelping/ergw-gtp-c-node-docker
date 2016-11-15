c354355666248f9139e721cddd81bbda6df34e34
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-15 17:51:26 +0100

    fix typos

--- ergw-gtp-c-start.sh
+++ ergw-gtp-c-start.sh
@@ -3,9 +3,9 @@
 . /etc/ergw-gtp-c-node/default-vars.sh
 
 # variable substitution in configuration files
-cat /opt/rel/ergw-gtp-r-node/releases/1.3.0/sys.config.templ | \
-    envsubst > /opt/rel/ergw-gtp-r-node/releases/1.3.0/sys.config
-cat /opt/rel/ergw-gtp-r-node/releases/1.3.0/vm.args.templ | \
-    envsubst > /opt/rel/ergw-gtp-r-node/releases/1.3.0/vm.args
+cat /opt/rel/ergw-gtp-c-node/releases/1.3.0/sys.config.templ | \
+    envsubst > /opt/rel/ergw-gtp-c-node/releases/1.3.0/sys.config
+cat /opt/rel/ergw-gtp-c-node/releases/1.3.0/vm.args.templ | \
+    envsubst > /opt/rel/ergw-gtp-c-node/releases/1.3.0/vm.args
 
-ergw-gtp-c-node foreground
+/opt/rel/ergw-gtp-c-node/bin/ergw-gtp-c-node foreground
