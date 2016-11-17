9cc48a36fcbfdaea178bac37c863ff69b04395a0
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-17 18:06:37 +0100

    change conf file path

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM grundrausch3n/ergw-gtp-c-node-base
 
 WORKDIR /
 COPY ergw-gtp-c-start.sh /usr/bin/
-COPY default-vars.sh /etc/ergw-gtp-c-node/
+COPY default-vars.sh /config/ergw-gtp-c-node/
 # Copy templates
-COPY config/sys.config.templ /etc/ergw-gtp-c-node/
-COPY config/vm.args.templ /etc/ergw-gtp-c-node/
+COPY config/sys.config.templ /config/ergw-gtp-c-node/
+COPY config/vm.args.templ /config/ergw-gtp-c-node/
 
 CMD ["ergw-gtp-c-start.sh"]
--- ergw-gtp-c-start.sh
+++ ergw-gtp-c-start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-. /etc/ergw-gtp-c-node/default-vars.sh
+. /config/ergw-gtp-c-node/default-vars.sh
 
 # variable substitution in configuration files
 RELPATH=$(find /opt/rel/ergw-gtp-c-node/releases/ -mindepth 1 -maxdepth 1 -type d)
-cat /etc/ergw-gtp-c-node/sys.config.templ | \
+cat /config/ergw-gtp-c-node/sys.config.templ | \
     envsubst > $RELPATH/sys.config
-cat /etc/ergw-gtp-c-node/vm.args.templ | \
+cat /config/ergw-gtp-c-node/vm.args.templ | \
     envsubst > $RELPATH/vm.args
 
 /opt/rel/ergw-gtp-c-node/bin/ergw-gtp-c-node foreground
